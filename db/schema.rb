f429e73672068f8b632d2206779385e530579086
DaniilKhozov <79273814+DaniilKhozov@users.noreply.github.com>
2021-03-01 16:40:50 +0300

    Add files via upload

---
+++ db/schema.rb
@@ -0,0 +1,85 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `rails
+# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2020_04_28_101410) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "orders", force: :cascade do |t|
+    t.integer "count"
+    t.bigint "store_id", null: false
+    t.bigint "product_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.bigint "warehouse_id", null: false
+    t.integer "status", default: 0
+    t.index ["product_id"], name: "index_orders_on_product_id"
+    t.index ["store_id"], name: "index_orders_on_store_id"
+    t.index ["warehouse_id"], name: "index_orders_on_warehouse_id"
+  end
+
+  create_table "products", force: :cascade do |t|
+    t.string "title"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.integer "products_count", default: 0
+  end
+
+  create_table "products_warehouses", force: :cascade do |t|
+    t.bigint "warehouse_id", null: false
+    t.bigint "product_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.integer "products_count", default: 0
+    t.index ["product_id"], name: "index_products_warehouses_on_product_id"
+    t.index ["warehouse_id"], name: "index_products_warehouses_on_warehouse_id"
+  end
+
+  create_table "stores", force: :cascade do |t|
+    t.string "title"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_stores_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  create_table "warehouses", force: :cascade do |t|
+    t.string "title"
+    t.integer "number"
+    t.string "address"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_warehouses_on_user_id"
+  end
+
+  add_foreign_key "orders", "products"
+  add_foreign_key "orders", "stores"
+  add_foreign_key "orders", "warehouses"
+  add_foreign_key "stores", "users"
+  add_foreign_key "warehouses", "users"
+end
---
+++ db/seeds.rb
@@ -0,0 +1,31 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+# Product.create([{title: 'Best'}])
+# 10.times do
+#   User.create!(
+#       email: Faker::Internet.email,
+#       first_name: Faker::Name.first_name,
+#       last_name: Faker::Name.last_name,
+#       password: "123456"
+#   )
+# end
+#
+# 20.times do
+#   Warehouse.create(
+#       title: Faker::Commerce.product_name,
+#       address: Faker::Address.street_address,
+#       number: Faker::Number.within(range: 5..30),
+#       user_id: Faker::Number.within(range: 6..17)
+#   )
+# end
+# 800.times do
+#   Store.create(
+#     user_id: Faker::Number.within(range: 6..17),
+#     title: Faker::Restaurant.name
+# )
+# end
